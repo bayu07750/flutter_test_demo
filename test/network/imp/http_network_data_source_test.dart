bce82a560d916048123aa7d321260535fd778efc
bayu07750 <bayupratama07750@gmail.com>
2025-02-16 11:40:52 +0700

    ignore variable that might be not used

--- test/network/imp/http_network_data_source_test.dart
+++ test/network/imp/http_network_data_source_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_test_demo/network/imp/http_network_data_source.dart';
 import 'package:flutter_test_demo/network/network_config.dart';
